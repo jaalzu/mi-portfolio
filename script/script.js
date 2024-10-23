1232a5b1c47130f2340ab3d10ab6b37e6502adab
Javier <javieralzuu@gmail.com>
2024-10-23 02:23:10 -0300

    footer done

--- script/script.js
+++ script/script.js
@@ -4,3 +4,36 @@ let navList = document.getElementById('nav-list');
 menu.addEventListener('click',() => {
     navList.classList.toggle('show-menu');
 })
+
+
+
+
+
+const copyToClipboard = element => {
+    const textToCopy = element.innerHTML;
+
+    const el = document.createElement("textarea");
+    el.value = textToCopy;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+
+    const tooltip = document.getElementById('tooltip');
+    tooltip.innerText = '¡Copiado!';
+    setTimeout(() => {
+        tooltip.innerText = 'Haz clic aquí para copiar'; // Volver al texto original después de un tiempo
+    }, 2000);
+}
+
+function showTooltip() {
+    const tooltip = document.getElementById('tooltip');
+    tooltip.style.visibility = 'visible';
+    tooltip.style.opacity = '1';
+}
+
+function resetTooltip() {
+    const tooltip = document.getElementById('tooltip');
+    tooltip.style.visibility = 'hidden';
+    tooltip.style.opacity = '0';
+}
